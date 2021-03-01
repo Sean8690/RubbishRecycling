c309c693dadb89e778273af50849058716a81332
Roshan Bassan <roshan.bassan@infotrack.com.au>
2021-03-02 10:47:58 +1100

    Removed unused code.

--- RubbishRecycling/api/ControllerModels/AuthenticationResponse.cs
+++ RubbishRecycling/api/ControllerModels/AuthenticationResponse.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using static RubbishRecyclingAU.ControllerModels.RequestAccess;
+﻿using static RubbishRecyclingAU.ControllerModels.RequestAccess;
 
 namespace RubbishRecyclingAU.ControllerModels
 {
@@ -14,40 +10,5 @@ namespace RubbishRecyclingAU.ControllerModels
     public class AuthenticationResponse : SessionPrivilegeLevelResponse
     {
         public string AntiforgeryHeaderValue { get; set; }
-        public string ApiAntiforgeryHeaderValue { get; set; }
-        public string MfaToken { get; set; }
-    }
-
-    public class LoginByTokenResponse : AuthenticationResponse
-    {
-        public bool Authenticated { get; set; }
-        public bool RequireRegistration { get; set; }
-        public bool IsLoggedInWithDifferentUser { get; set; }
-    }
-
-    public class TokenDetails
-    {
-        public string Email { get; set; }
-        public string Mobile { get; set; }
-        public bool EmailExistsButNotLinked { get; set; }
-        public bool EmailExistsAndLinked { get; set; }
-    }
-
-    public class LoginByTokenAuthResponse : AuthenticationResponse
-    {
-        public bool Authenticated { get; set; }
-        public bool SmsCodeValid { get; set; }
-    }
-
-    public class LoginByInfotrackAuthResponse : AuthenticationResponse
-    {
-        public bool Authenticated { get; set; }
-        public bool EmailExists { get; set; }
-        public bool CanBeLinkedToExistingUser { get; set; }
-        public string InfotrackUserEmail { get; set; }
-        public string InfortrackUserPhone { get; set; }
-        public bool SmsCodeValid { get; set; }
-        public string Message { get; set; }
-        public DateTime LoginLinkExpireTimeUtc { get; set; }
     }
 }
